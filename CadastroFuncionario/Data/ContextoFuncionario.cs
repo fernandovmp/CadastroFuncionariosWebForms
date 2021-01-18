2ec908636c79f5b38b277cf5794c32e730e2a9ef
Fernando Mariano Pereira <fernando.pereira@cadmus.com.br>
2021-01-18 16:17:33 -0300

    [Cadastro Funcionário] adiconar contexto de banco de dados para funcionário

---
+++ CadastroFuncionario/Data/ContextoFuncionario.cs
@@ -0,0 +1,13 @@
+﻿using CadastroFuncionario.Models;
+using System.Data.Entity;
+
+namespace CadastroFuncionario.Data
+{
+    public class ContextoFuncionario : DbContext
+    {
+        public ContextoFuncionario() : this(nameOrConnectionString: "CadastroFuncionario") { }
+        public ContextoFuncionario(string nameOrConnectionString) : base(nameOrConnectionString) { }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+    }
+}
