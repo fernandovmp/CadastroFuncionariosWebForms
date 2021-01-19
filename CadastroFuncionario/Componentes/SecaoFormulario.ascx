bd9c7fe724d209dfe8ef3ad17de03414f3e27ae0
Fernando Mariano Pereira <fernando.pereira@cadmus.com.br>
2021-01-19 15:31:58 -0300

    [Componentes] criar componente de seção de formulário genérico
    Cria um componente genérico para uma seção de formulário, um arquivo css com os estilos e importa os estilos na Site.master

---
+++ CadastroFuncionario/Componentes/SecaoFormulario.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SecaoFormulario.ascx.cs" Inherits="CadastroFuncionario.Componentes.SecaoFormulario" %>
+<section class="form-section">
+    <h4><%: Titulo %></h4>
+    <asp:PlaceHolder ID="contentPlaceHolder" runat="server" />
+</section>
---
+++ CadastroFuncionario/Componentes/SecaoFormulario.ascx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CadastroFuncionario.Componentes
+{
+    public partial class SecaoFormulario : System.Web.UI.UserControl
+    {
+        public string Titulo { get; set; }
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        [TemplateInstance(TemplateInstance.Single)]
+        public ITemplate Content { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Content != null)
+            {
+                Content.InstantiateIn(contentPlaceHolder);
+            }
+        }
+
+    }
+}
