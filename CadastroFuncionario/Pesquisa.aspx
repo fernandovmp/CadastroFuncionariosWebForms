094ca16ebc4dd562f9ce50b0638301c23b4244c9
Fernando Mariano Pereira <fernando.pereira@cadmus.com.br>
2021-01-20 15:42:08 -0300

    [Pesquisa] criar página de pesquisa de funcionário

---
+++ CadastroFuncionario/Pesquisa.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Pesquisa.aspx.cs" Inherits="CadastroFuncionario.Pesquisa" %>
+<%@ Register TagPrefix="cad" TagName="Popup" Src="~/Componentes/Popup.ascx" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3 class="text-center">PESQUISA DE FUNCIONÁRIOS</h3>
+    <section class="text-center">
+        <label>
+            CPF:
+            <asp:TextBox runat="server" ID="txtCpf" ClientIDMode="Static"/>
+        </label>
+         <div class="text-center" style="margin-top: 30px;">
+        <table class="button-group">
+            <tr>
+                <td>
+                    <asp:Button ID="btnPesquisar" CssClass="btn btn-success" runat="server" Text="Salvar" OnClick="btnPesquisar_Click"/>
+                </td>
+                <td>
+                    <asp:Button ID="btnLimpar" CssClass="btn" runat="server" Text="Limpar" OnClick="btnLimpar_Click" />
+                </td>
+                <td>
+                    <a href="Home.aspx" class="btn btn-danger">Voltar</a>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <cad:Popup runat="server" ID="popup" Titulo="Erro"/>
+    <script type="text/javascript">
+         jQuery(function ($) {
+             $("#txtCpf").mask("999.999.999-99");
+         });
+        }
+    </script>   
+    <style>
+        body {
+            padding: 0 20px;
+        }
+        .button-group {
+            display: inline;
+        }
+        .button-group .btn {
+            width: 100px;
+        }
+        .button-group tr td {
+            padding: 0 8px;
+        }
+    </style>
+    </section>
+</asp:Content>
---
+++ CadastroFuncionario/Pesquisa.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace CadastroFuncionario
+{
+
+
+    public partial class Pesquisa
+    {
+
+        /// <summary>
+        /// Controle txtCpf.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCpf;
+
+        /// <summary>
+        /// Controle btnPesquisar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPesquisar;
+
+        /// <summary>
+        /// Controle btnLimpar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLimpar;
+
+        /// <summary>
+        /// Controle popup.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::CadastroFuncionario.Componentes.Popup popup;
+    }
+}
