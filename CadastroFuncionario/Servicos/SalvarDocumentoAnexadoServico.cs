08122214436f72f5abbec8d4ad083482d39141b9
Fernando Mariano Pereira <fernando.pereira@cadmus.com.br>
2021-01-20 15:28:09 -0300

    [Upload] adicionar classe para salvar documentos anexados

---
+++ CadastroFuncionario/Servicos/SalvarDocumentoAnexadoServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace CadastroFuncionario.Servicos
+{
+    public class SalvarDocumentoAnexadoServico
+    {
+        public SalvarDocumentoAnexadoServico(HttpPostedFile documento)
+        {
+            Documento = documento;
+            string extensaoDocumento = Path.GetExtension(documento.FileName);
+            string nomeDocumentoSemExtensao = Path.GetFileNameWithoutExtension(documento.FileName);
+            NomeDocumento = $"{nomeDocumentoSemExtensao}-{Guid.NewGuid()}{extensaoDocumento}";
+        }
+
+        public string NomeDocumento { get; private set; }
+        public HttpPostedFile Documento { get; private set; }
+
+        public void SalvarDocumento(string raizDoCaminhoDestino)
+        {
+            string caminhoParaSalvar = Path.Combine(raizDoCaminhoDestino, NomeDocumento);
+            Directory.CreateDirectory(raizDoCaminhoDestino);
+            Documento.SaveAs(caminhoParaSalvar);
+        }
+    }
+}
