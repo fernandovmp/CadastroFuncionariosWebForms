833dc050f15d7c190498150aaf1ea008502c668f
Fernando Mariano Pereira <fernando.pereira@cadmus.com.br>
2021-01-25 14:25:56 -0300

    [CadastroFuncionarios] configurar container de injeção de dependência

--- CadastroFuncionario/Global.asax.cs
+++ CadastroFuncionario/Global.asax.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using CadastroFuncionario.Data.Factories;
+using CadastroFuncionario.Logger;
+using CadastroFuncionarios.CepModulo;
+using Microsoft.AspNet.WebFormsDependencyInjection.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using Unity;
 
 namespace CadastroFuncionario
 {
@@ -16,6 +22,14 @@ namespace CadastroFuncionario
             // Código que é executado na inicialização do aplicativo
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IUnityContainer container = this.AddUnity();
+            container.RegisterType<ICepServico, BrasilApiCepServico>();
+            container.RegisterSingleton<ILogger, TxtLogger>();
+
+            string connectionString = WebConfigurationManager.ConnectionStrings["CadastroFuncionario"].ConnectionString;
+            var contextoFuncionarioFactory = new ContextoFuncionarioFactory(connectionString);
+            container.RegisterInstance<IContextoFuncionarioFactory>(contextoFuncionarioFactory);
         }
     }
 }
